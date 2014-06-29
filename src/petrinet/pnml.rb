c3e9ed6f1be0bbb5470fe55d1618c98f5ab0dbbe
ryysd <ry.ysd01@gmail.com>
2014-06-29 11:43:40 +0900

    implement csv dumper

--- src/petrinet/pnml.rb
+++ src/petrinet/pnml.rb
@@ -1,4 +1,5 @@
 require 'pp'
+require 'matrix'
 require 'active_support/core_ext'
 
 class PetrinetNode
@@ -12,6 +13,10 @@ class PetrinetNode
 
   def add_arc(arc)
     @arcs.push arc
+  end
+
+  def arc(target)
+    @arcs.find{|a| target.id == a.target.id}
   end
 end
 
@@ -47,6 +52,44 @@ class Petrinet
   def initialize(places, transitions)
     @places = places
     @transitions = transitions
+
+    @incidence_matrix, @input_matrix, @output_matrix = create_matrix
+  end
+
+  def create_matrix
+    row_length = @places.length
+    col_length = @transitions.length
+    input_matrix_arr = (Array.new row_length).map{Array.new col_length}
+    output_matrix_arr = (Array.new row_length).map{Array.new col_length}
+
+    @places.each.with_index do |place, i|
+      @transitions.each.with_index do |transition, j|
+	input_arc = transition.arc place
+	output_arc = place.arc transition
+
+	input_matrix_arr[i][j] = input_arc.nil? ? 0 : input_arc.inscription
+	output_matrix_arr[i][j] = output_arc.nil? ? 0 : output_arc.inscription
+      end
+    end
+
+    input_matrix = Matrix.rows input_matrix_arr
+    output_matrix = Matrix.rows output_matrix_arr
+    [input_matrix - output_matrix, input_matrix, output_matrix]
+  end
+
+  def csv
+    col = @transitions.map{|t| t.name}
+    row = @places.map{|p| p.name}
+
+    incidence_csv = [((['incidence'] + col).join ',')]
+    input_csv     = [((['input'] + col).join ',')]
+    output_csv    = [((['output'] + col).join ',')]
+
+    incidence_csv += @incidence_matrix.row_vectors.map.with_index{|v, i| (v.to_a.unshift row[i]).join ','}
+    input_csv += @input_matrix.row_vectors.map.with_index{|v, i| (v.to_a.unshift row[i]).join ','}
+    output_csv += @output_matrix.row_vectors.map.with_index{|v, i| (v.to_a.unshift row[i]).join ','}
+
+    [incidence_csv, input_csv, output_csv].map{|c| c.join "\n"}
   end
 
   def dot
@@ -88,4 +131,17 @@ class PNML
 end
 
 petrinet = PNML.parse (File.open './tmp/pnml.xml').read
-puts petrinet.dot
+incidence_csv, input_csv, output_csv = petrinet.csv
+
+File.open './tmp/incidence.csv', 'w' do |file|
+  file.write incidence_csv
+end
+
+File.open './tmp/input.csv', 'w' do |file|
+  file.write input_csv
+end
+
+File.open './tmp/output.csv', 'w' do |file|
+  file.write output_csv
+end
+#puts petrinet.dot
