b4ea1933a470e483e12923ac1c3e8e735c93263d
ryysd <ry.ysd01@gmail.com>
2014-06-29 20:14:41 +0900

    implement PNML#pipe_form_xml? method

--- src/petrinet/pnml.rb
+++ src/petrinet/pnml.rb
@@ -1,13 +1,19 @@
 require 'active_support/core_ext'
 
 class PNML
-  def self.parse(xml, pipe = true)
+  def self.pipe_form_pnml?(xml_hash)
+    xml_hash['pnml']['net']['type'] == 'P/T net'
+  end
+
+  def self.parse(xml)
     hash =  Hash.from_xml xml
+    pipe = pipe_form_pnml? hash
+
     net = pipe ? hash['pnml']['net'] : hash['pnml']['net']['page']
     value_key = pipe ? 'value' : 'text'
 
     places = net['place'].map{|p| Place.new p['id'], p['name'][value_key], (p.has_key? 'initialMarking') ? (p['initialMarking'][value_key].gsub(/[^0-9]/, "").to_i) : 0}
-    transitions = net['transition'].map{|t| Transition.new t['id'], t['name'][value_key]}
+    transitions = net['transition'].map{|t| Transition.new t['id'], (t.has_key? 'name') ? t['name'][value_key] : t['id']}
 
     nodes = places + transitions
     net['arc'].each do |a| 
