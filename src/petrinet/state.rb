74dbe4d9095d63e16d0255dcd088c7c3424a043e
ryysd <ry.ysd01@gmail.com>
2014-06-30 12:20:11 +0900

    reduce all? method

--- src/petrinet/state.rb
+++ src/petrinet/state.rb
@@ -3,12 +3,14 @@ class State
 
   def initialize(marking)
     @marking = marking
+    @hash = ''
   end
 
   def successors(incidence_column_vectors, transitions)
     (0...transitions.length).map do |i|
-      new_marking = (@marking.zip incidence_column_vectors[i]).map{|(a,b)| a+b}
-      new_marking.all?{|m| m >= 0} ? [transitions[i], (State.new new_marking)] : nil
+      valid = true
+      new_marking = (@marking.zip incidence_column_vectors[i]).map{|(a,b)| e=a+b; valid=valid&&e>=0; e}
+      valid ? [transitions[i], (State.new new_marking)] : nil
     end.compact
   end
 
@@ -17,6 +19,7 @@ class State
   end
 
   def to_s
-    @marking.to_a.to_s.gsub ' ', ''
+    @hash = @marking.to_a.to_s.gsub ' ', '' if @hash.empty?
+    @hash
   end
 end
