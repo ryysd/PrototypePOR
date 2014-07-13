96a5d21dfb9e576b1a6ac33b943bec77123027da
ryysd <ry.ysd01@gmail.com>
2014-07-13 21:20:44 +0900

    implement profiler

--- c/src/util/ats_file_reader.h
+++ c/src/util/ats_file_reader.h
@@ -10,7 +10,11 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iomanip>
+#include <chrono>
 #include <map>
+#include <queue>
 #include "../../thirdparty/picojson.h"
 #include "../../thirdparty//logger.h"
 
@@ -28,6 +32,110 @@ std::vector<std::string> split(const std::string& str, char delim){
   res.push_back(std::string(str, current, str.size() - current));
   return res;
 }
+
+class Profiler {
+ public:
+   Profiler() {}
+   ~Profiler() { for (auto kv : timers_) { delete kv.second; } }
+
+  void Start(const std::string& name) {
+    if (timers_.find(name) == timers_.end()) timers_.insert(std::make_pair(name, new ProfilerTimer()));
+    timers_.at(name)->Start();
+  }
+
+  void Stop(const std::string& name) {
+    timers_.at(name)->Stop();
+  }
+
+  void Dump() {
+    unsigned column_width = 15;
+    unsigned fix_width = 2;
+    std::string separator = " | ";
+    std::vector<std::string> labels{"name", "incl_ratio [%]", "incl_total [ms]", "incl_ave [ms]", "call"};
+    float total_time = timers_.at("total")->total();
+    std::stringstream sstream;
+
+    sstream << MakeLabels(labels, column_width, separator) << std::endl;
+
+    auto comp = [](DumpObject a, DumpObject b) { return a.inclusive_time_ratio() > b.inclusive_time_ratio(); };
+    std::priority_queue<DumpObject, std::vector<DumpObject>, decltype(comp)> dump_objects(comp);
+    float real_total_time = 0;
+    for (auto kv : timers_) {
+      std::string name = kv.first;
+      dump_objects.push(DumpObject(kv.first, kv.second->average(), kv.second->total(), ((kv.second->total() * 100) / total_time), kv.second->call_count()));
+      real_total_time += kv.second->total();
+    }
+    float other_total_time = total_time - (real_total_time - total_time);
+    dump_objects.push(DumpObject("other", 0, other_total_time, ((other_total_time * 100) / total_time), 0));
+
+    while(!dump_objects.empty()) {
+      sstream << dump_objects.top().ToString(column_width, fix_width, separator) << std::endl;
+      dump_objects.pop();
+    }
+
+    std::cout << sstream.str();
+  }
+
+ private:
+  std::string MakeLabels(const std::vector<std::string>& labels, unsigned column_width, const std::string& separator) const {
+    std::stringstream ss;
+    for (auto label : labels) { ss << std::setw(column_width) << label << separator; }
+
+    return ss.str();
+  }
+
+  class ProfilerTimer {
+   public:
+    ProfilerTimer() : total_(0), call_count_(0) {}
+    void Start() { start_ = std::chrono::high_resolution_clock::now(); }
+    void Stop() { total_ += std::chrono::high_resolution_clock::now() - start_; call_count_++; }
+
+    float total() const { return ((float)std::chrono::duration_cast<std::chrono::microseconds>(total_).count()) / 1000; }
+    unsigned call_count() const { return call_count_; }
+    float average() const { return total() / call_count_; }
+    
+   private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::chrono::high_resolution_clock::duration total_;
+    unsigned call_count_;
+  };
+
+  class DumpObject {
+   public:
+    DumpObject(std::string name, float inclusive_average_time, float inclusive_total_time, float inclusive_time_ratio, unsigned call_count)
+      : name_(name), inclusive_averagel_time_(inclusive_average_time), inclusive_total_time_(inclusive_total_time), inclusive_time_ratio_(inclusive_time_ratio), call_count_(call_count) {}
+
+    const std::string& name() const { return name_; }
+    float inclusive_averate_time() const { return inclusive_averagel_time_; }
+    float inclusive_total_time() const { return inclusive_total_time_; }
+    float inclusive_time_ratio() const { return inclusive_time_ratio_; }
+    unsigned call_count() const { return call_count_; }
+
+    std::string ToString(unsigned column_width, unsigned fix_width, std::string separator) const {
+      std::stringstream ss;
+
+      ss << std::fixed << std::setprecision(fix_width) << 
+        std::setw(column_width) << name_ << separator << 
+        std::setw(column_width) << inclusive_time_ratio_ << separator << 
+        std::setw(column_width) << inclusive_total_time_ << separator << 
+        std::setw(column_width) << inclusive_averagel_time_ << separator << 
+        std::setw(column_width) << call_count_ << separator;
+
+      return ss.str();
+    }
+
+   private:
+    std::string name_;
+    float inclusive_averagel_time_;
+    float inclusive_total_time_;
+    float inclusive_time_ratio_;
+    unsigned call_count_;
+  };
+
+  std::map<std::string, ProfilerTimer*> timers_;
+
+  DISALLOW_COPY_AND_ASSIGN(Profiler);
+};
 
 typedef std::vector<std::string> EntitySet;
 
@@ -121,26 +229,23 @@ class StateSpace {
     FreeTransitions();
   }
 
-  void Register(State* state) { states_.insert(make_pair(state->name(), state)); }
+  State* Register(State* state) { return states_.insert(make_pair(state->name(), state)).first->second; }
   bool isRegistered(const std::string& name) const { return states_.find(name) != states_.end();  }
 
-  State* Create(const std::string& name, const std::vector<std::string>& entities, bool is_init = false) {
-    const State* old = FindByName(name);
-
-    if (old == NULL) {
-      State* state = new State(name, entities);
-      Register(state);
-      if (is_init) init_state_ = state;
-
-      return state;
-    }
-
-    return const_cast<State*>(old);
+  State* Create(const std::string& name, const std::vector<std::string>& entities) {
+    State* old = FindByName(name);
+    return old == NULL ? Register(new State(name, entities)) : old;
   }
 
-  const State* FindByName(const std::string& name) const {
+  State* Create(const std::string& name, const std::vector<std::string>& entities, bool is_init) {
+    State* state = Create(name, entities);
+    if (is_init) init_state_ = state;
+    return state;
+  }
+
+  State* FindByName(const std::string& name) const {
     auto it = states_.find(name);
-    return (it != states_.end()) ? reinterpret_cast<const State*>(it->second) : NULL;
+    return (it != states_.end()) ? it->second : NULL;
   }
 
   const std::map<std::string, State*>& states() const { return states_; }
@@ -164,30 +269,25 @@ class Vector {
 
 class ActionTable {
  public:
-  ~ActionTable() { for (auto &kv : actions_) { delete kv.second; }}
+  ~ActionTable() { FreeActions(); }
 
-  void Register(Action* action) { actions_.insert(make_pair(action->name(), action)); }
+  Action* Register(Action* action) { return actions_.insert(make_pair(action->name(), action)).first->second; }
+
   Action* Create(const std::string& name, const EntitySet& creator, const EntitySet& eraser, const EntitySet& reader, const EntitySet& embargoes) {
-    const Action* old = FindByName(name);
-
-    if (old == NULL) {
-      Action* action = new Action(name, creator, eraser, reader, embargoes);
-      Register(action);
-
-      return action;
-    }
-
-    return const_cast<Action*>(old);
+    Action* old = FindByName(name);
+    return old == NULL ? Register(new Action(name, creator, eraser, reader, embargoes)) : old;
   }
 
-  const Action* FindByName(const std::string& name) const {
+  Action* FindByName(const std::string& name) const {
     auto it = actions_.find(name);
-    return (it != actions_.end()) ? reinterpret_cast<const Action*>(it->second) : NULL;
+    return (it != actions_.end()) ? it->second : NULL;
   }
 
   const std::map<std::string, Action*> actions() const { return actions_; }
 
  private:
+  void FreeActions() { for (auto &kv : actions_) { delete kv.second; } }
+
   std::map<std::string, Action*> actions_;
 };
 
@@ -277,6 +377,9 @@ class ATSFileReader {
 
  public:
   static const std::pair<StateSpace*, ActionTable*> Read(std::string fname) {
+    Profiler profiler;
+    profiler.Start("total");
+
     std::ifstream ifs(fname);
 
     if (ifs.fail()) {
@@ -284,19 +387,35 @@ class ATSFileReader {
       return std::make_pair<StateSpace*, ActionTable*>(NULL, NULL);
     }
 
+    profiler.Start("read");
     std::string json((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    profiler.Stop("read");
+
     picojson::value json_value;
     std::string err;
 
+    profiler.Start("parse");
     picojson::parse(json_value, json.data(), json.data() + json.length(), &err);
+    profiler.Stop("parse");
 
     if (!err.empty()) {
       ERROR(err.c_str());
       return std::make_pair<StateSpace*, ActionTable*>(NULL, NULL);
     }
 
+    profiler.Start("gen_actions");
     ActionTable* action_table = ATSFileReader::CreateActionTable(json_value);
+    profiler.Stop("gen_actions");
+
+    profiler.Start("gen_states");
     StateSpace* state_space = ATSFileReader::CreateStateSpace(json_value, action_table);
+    profiler.Stop("gen_states");
+
+    std::cout << action_table->actions().size() << std::endl;
+    std::cout << state_space->states().size() << std::endl;
+
+    profiler.Stop("total");
+    profiler.Dump();
 
     return std::make_pair(state_space, action_table);
   }
