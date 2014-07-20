78776c08d0ff13bf7d4a2f9718febf01969b1974
ryysd <ry.ysd01@gmail.com>
2014-07-20 18:19:02 +0900

    add command line parser

--- c/src/main.cc
+++ c/src/main.cc
@@ -5,18 +5,21 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include "../thirdparty/prettyprint.h"
+#include "../thirdparty/cmdline.h"
 #include "./atS_file_reader.h"
 
-int main() {
-  auto pair = ATSFileReader::Read("../tmp/FMS-2/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/Philosophers-5/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/Philosophers-10/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/sample_medium/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/sample/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/sample_small/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/Philosopher_02/ats.json");
-  // auto pair = ATSFileReader::Read("../tmp/interleave_3/ats.json");
+int main(int argc, char** argv) {
+  cmdline::parser parser;
+
+  parser.add<std::string>("ats", 'a');
+  if (!parser.parse(argc, argv)) {
+    std::cout << parser.error_full() << parser.usage();
+  }
+
+  auto pair = ATSFileReader::Read(parser.get<std::string>("ats"));
+  // auto pair = ATSFileReader::Read("../tmp/FMS-2/ats.json");
   // pair.first->dump();
   std::cout << "// " << pair.first->states().size() << std::endl;
 }
